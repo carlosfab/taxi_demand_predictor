a6a87f97c8e6a5474a7a6fc3e0ec5431dff3c04c
Carlos Melo <carlos.fab@gmail.com>
2023-09-30 09:30:24 -0300

    fixed plot function

--- src/plot.py
+++ src/plot.py
@@ -3,6 +3,7 @@ from typing import Optional
 from datetime import timedelta
 
 import pandas as pd
+import plotly.graph_objects as go  # <-- Add this import
 import plotly.express as px
 
 
@@ -11,7 +12,7 @@ def plot_one_sample(
     features: pd.DataFrame,
     targets: Optional[pd.Series] = None,
     predictions: Optional[pd.Series] = None
-) -> px.Figure:
+):
     """
     Plots a sample with historical rides, actual values, and predicted values.
 
@@ -25,26 +26,33 @@ def plot_one_sample(
     - A Plotly Figure object.
     """
     # Extract data
-    sample_features = features.iloc[example_id]
-    target_value = targets.iloc[example_id] if targets is not None else None
+    features_ = features.iloc[example_id]
+    target_ = targets.iloc[example_id] if targets is not None else None
 
     # Extract time series data
-    ts_columns = [col for col in features.columns if col.startswith('rides_previous_')]
-    ts_values = [sample_features[col] for col in ts_columns] + [target_value]
-
+    ts_columns = [c for c in features.columns if c.startswith('rides_previous_')]
+    ts_values = [features_[c] for c in ts_columns] + [target_]
     ts_dates = pd.date_range(
-        start=sample_features['pickup_hour'] - timedelta(hours=len(ts_columns)),
-        end=sample_features['pickup_hour'],
+        features_['pickup_hour'] - timedelta(hours=len(ts_columns)),
+        features_['pickup_hour'],
         freq='H'
     )
 
+    # line plot with past values
+    title = f'Pick up hour={features_["pickup_hour"]}, location_id={features_["pickup_location_id"]}'
+    fig = px.line(
+        x=ts_dates, y=ts_values,
+        template='plotly_dark',
+        markers=True, title=title
+    )
+
     # Create the figure
-    title = f'Pick up hour={sample_features["pickup_hour"]}, location_id={sample_features["pickup_location_id"]}'
+    title = f'Pick up hour={features_["pickup_hour"]}, location_id={features_["pickup_location_id"]}'
     fig = px.line(x=ts_dates, y=ts_values, template='plotly_dark', markers=True, title=title)
 
     # Add actual and predicted values (if available)
     if targets is not None:
-        fig.add_scatter(x=ts_dates[-1:], y=[target_value], line_color='green', mode='markers', marker_size=10, name='actual value')
+        fig.add_scatter(x=ts_dates[-1:], y=[target_], line_color='green', mode='markers', marker_size=10, name='actual value')
     if predictions is not None:
         predicted_value = predictions.iloc[example_id]
         fig.add_scatter(x=ts_dates[-1:], y=[predicted_value], line_color='red', mode='markers', marker_symbol='x', marker_size=15, name='prediction')
